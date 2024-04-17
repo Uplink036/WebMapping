0df2f5249217c273efc6eee139996c88b0f8294e
Uplink036 <Oliversjodin02@gmail.com>
2024-04-17 15:53:56 +0200

    Refacto web scrapper code into functions

--- scrapper.py
+++ scrapper.py
@@ -1,19 +1,32 @@
 import requests 
 from bs4 import BeautifulSoup 
 
-URL = "https://scrapeme.live/shop/"
-headers = {'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"} 
-# Here the user agent is for Edge browser on windows 10. You can find your browser user agent from the above given link. 
-html_response = requests.get(url=URL, headers=headers)  
-raw_html = html_response.content
-soup = BeautifulSoup(raw_html, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib 
-print(soup.prettify()) 
+def getHTMLResponse(url):
+    headers = {'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"} 
+    html_response = requests.get(url=URL, headers=headers)
+    return html_response
+
+def getAllLinks(html_response):
+    soup = BeautifulSoup(html_response.content, 'html5lib')
+    links = []
+    for link in soup.find_all('a'):
+        links.append(link.get('href'))
+    return links
+
+def printRawHTML(html_response):
+    raw_html = html_response.content
+    print(raw_html)
+
+def printParsedHTML(raw_html):
+    soup = BeautifulSoup(raw_html, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib 
+    print(soup.prettify())
 
 
-links = []
 
-# Find all the links in the page
-for link in soup.find_all('a'):
-    links.append(link.get('href'))
+if __name__ == "__main__":
+    URL = "https://scrapeme.live/shop/"
+    html_response = getHTMLResponse(URL)
+    printParsedHTML(html_response.content)
 
-print(links)
+    links = getAllLinks(html_response)
+    print(links)
