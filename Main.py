0f790702f3e3fdfda2e8305ad0dc4fdff0143398
Uplink036 <Oliversjodin02@gmail.com>
2024-04-17 17:01:43 +0200

    Add directed graph

--- Main.py
+++ Main.py
@@ -1,17 +1,14 @@
-
-from links import links_to_websites
-from Map import plot_web_map
+from Map import plot_nodes
 from Node import Node
 from Scrapper import getHTMLResponse, getSoup, printParsedHTML, getAllLinks
 
 
 def main():
-    URL = "https://scrapeme.live/shop/"
+    URL = "https://deviceatlas.com/blog/list-of-user-agent-strings#desktop"
     websites = {}
     urls = [URL]
-    while len(urls) > 0:
+    while len(urls) > 0 and len(websites) < 100:
         url = urls.pop(0)
-        
         website_name = getNameFromURL(url)
         if website_name not in websites:
             websites[website_name] = Node(website_name)
@@ -24,26 +21,27 @@ def main():
         links = getAllLinks(soup)
 
         for link in links:
-            website_name = getNameFromURL(link)
-            if link is not None:
+            link_website_name = getNameFromURL(link)
+            if link_website_name is not None:
                 urls.append(link)
-                if website_name not in websites:
+                if link_website_name not in websites:
                     urls.append(link)
-                    websites[website_name] = Node(website_name)
-                websites[website_name].add_edge(websites[website_name])
-
-        websites[website_name].visited = True
-    
+                    websites[link_website_name] = Node(link_website_name)
+                websites[website_name].add_edge(websites[link_website_name])
 
             
+    plot_nodes(websites[getNameFromURL(URL)])
 
 
 def getNameFromURL(url):
     if not checkIfURL(url):
         return None
-    return url.split("//")[-1].split("/")[0]
+    return url.split("//")[-1]
 
 def checkIfURL(url):
+    
+    if url is None:
+        return False
     return url.startswith("http://") or url.startswith("https://")
 
 if __name__ == "__main__":
