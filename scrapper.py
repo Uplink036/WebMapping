18ce045ffae4fa998bb9886a3a65c9c3073a9d20
Uplink036 <Oliversjodin02@gmail.com>
2024-04-17 16:15:33 +0200

    Refactor main out from Scrapper

--- scrapper.py
+++ scrapper.py
@@ -1,13 +1,16 @@
 import requests 
 from bs4 import BeautifulSoup 
-
+from map import plot_web_map
 def getHTMLResponse(url):
     headers = {'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"} 
     html_response = requests.get(url=URL, headers=headers)
     return html_response
 
-def getAllLinks(html_response):
+def getSoup(html_response):
     soup = BeautifulSoup(html_response.content, 'html5lib')
+    return soup
+
+def getAllLinks(soup):
     links = []
     for link in soup.find_all('a'):
         links.append(link.get('href'))
@@ -21,12 +24,3 @@ def printParsedHTML(raw_html):
     soup = BeautifulSoup(raw_html, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib 
     print(soup.prettify())
 
-
-
-if __name__ == "__main__":
-    URL = "https://scrapeme.live/shop/"
-    html_response = getHTMLResponse(URL)
-    printParsedHTML(html_response.content)
-
-    links = getAllLinks(html_response)
-    print(links)
