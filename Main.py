910e6d6620a93d122240784638578e6fed007721
Uplink036 <Oliversjodin02@gmail.com>
2024-04-18 14:37:39 +0200

    Add check for visited websites

--- Main.py
+++ Main.py
@@ -6,14 +6,17 @@ from Scrapper import getHTMLResponse, getSoup, printParsedHTML, getAllLinks
 def main():
     URL = "https://deviceatlas.com/blog/list-of-user-agent-strings#desktop"
     websites = {}
+    visited_websites = {}
     urls = [URL]
     while len(urls) > 0 and len(websites) < 100:
         url = urls.pop(0)
         website_name = getNameFromURL(url)
         if website_name not in websites:
             websites[website_name] = Node(website_name)
-        else:
+        
+        if website_name in visited_websites:
             continue
+
 
 
         html_response = getHTMLResponse(url)
@@ -25,10 +28,10 @@ def main():
             if link_website_name is not None:
                 urls.append(link)
                 if link_website_name not in websites:
-                    urls.append(link)
                     websites[link_website_name] = Node(link_website_name)
                 websites[website_name].add_edge(websites[link_website_name])
-
+        
+        visited_websites[website_name] = True
             
     plot_nodes(websites[getNameFromURL(URL)])
 
