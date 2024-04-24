63bce1b4464cc89fc76a2b90daccdc2d5e519821
Uplink036 <Oliversjodin02@gmail.com>
2024-04-24 11:27:48 +0000

    Add test_node.py for Node unit tests

---
+++ test/test_node.py
@@ -0,0 +1,15 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import pytest
+from src.Node import Node
+
+class TestNode():
+    def test_init_node():
+        node = Node("www.google.com")
+        assert node.website == "www.google.com"
+        assert node.edges == []
+        assert node.visited == False
+
+if __name__ == "__main__":
+    pytest.main()
