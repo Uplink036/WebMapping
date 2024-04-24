694280a3a477eced1705bb4810e45bd290745ff1
Uplink036 <Oliversjodin02@gmail.com>
2024-04-24 10:53:10 +0000

    Renamed checkIfURL to isValidURL

--- src/Url_Handling.py
+++ src/Url_Handling.py
@@ -1,9 +1,9 @@
 def get_name_from_URL(url):
-    if not checkIfURL(url):
+    if not isValidURL(url):
         return None
     return url.split("//")[-1]
 
-def checkIfURL(url):
+def isValidURL(url):
     if url is None:
         return False
     return url.startswith("http://") or url.startswith("https://")
