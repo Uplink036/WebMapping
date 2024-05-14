2ea1bda6d473f431839ecd8d8ca9d58f9587bd34
Uplink036 <oliversjodin02@gmail.com>
2024-05-14 20:25:12 +0000

    Add hypothesis to scrapper

--- test/test_scrapper.py
+++ test/test_scrapper.py
@@ -1,11 +1,14 @@
 import pytest
+from hypothesis import given, assume
+from hypothesis import strategies as st
 from unittest import mock
 
 from webmap.Scrapper import get_HTML_response, get_soup, get_all_links, print_parsed_HTML, print_raw_HTML
 
 class TestScrapper():
-    def test_get_HTML_response(self):
-        url = "https://www.google.com"
+
+    @given(st.from_regex(r"https://."))
+    def test_get_HTML_response(self, url):
         with mock.patch('webmap.Scrapper.requests.get') as mock_get:
             get_HTML_response(url)
             mock_get.assert_called_with(url=url, headers={'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"})
