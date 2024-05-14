7bf44077252ca1556e06d60f9bf48804c15a1b3e
Uplink036 <oliversjodin02@gmail.com>
2024-05-14 20:32:06 +0000

    Add hypothesis to url_handling

--- test/test_url_handling.py
+++ test/test_url_handling.py
@@ -1,4 +1,6 @@
 import pytest 
+from hypothesis import given, assume
+from hypothesis import strategies as st
 from webmap.Url_Handling import get_name_from_URL, isValidURL
 
 class TestURLHandling():
@@ -8,11 +10,19 @@ class TestURLHandling():
         assert isValidURL("www.google.com") == False
         assert isValidURL(None) == False
 
+    @given(url=st.text())
+    def test_fuzz_isValidURL(self, url):
+        isValidURL(url=url)
+
     def test_get_name_from_URL(self):
         assert get_name_from_URL("https://www.google.com") == "www.google.com"
         assert get_name_from_URL("http://www.google.com") == "www.google.com"
         assert get_name_from_URL("www.google.com") == None
         assert get_name_from_URL(None) == None
+
+    @given(url=st.text())
+    def test_fuzz_get_name_from_URL(self, url):
+        get_name_from_URL(url=url)
 
 if __name__ == "__main__":
     pytest.main()
