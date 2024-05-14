f846da7c79f007c47c7319e047a5ba32faf288a5
Uplink036 <oliversjodin02@gmail.com>
2024-05-14 20:06:31 +0000

    Add hypothesis to node

--- test/test_node.py
+++ test/test_node.py
@@ -1,47 +1,62 @@
 import pytest
+from hypothesis import given, assume
+from hypothesis import strategies as st
 from webmap.Node import Node
 
 class TestNode():
-    def test_init_node(self):
-        node = Node("www.google.com")
-        assert node.website == "www.google.com"
+    @given(st.text())
+    def test_init_node(self, website):
+        try:
+            node = Node(website)
+        except ValueError:
+            return
+        assert node.website == website
         assert node.edges == {}
         assert node.visited == False
 
-    def test_add_edge(self):
-        node1 = Node("www.google.com")
-        node2 = Node("www.facebook.com")
+    @given(st.text(min_size=1), st.text(min_size=1))
+    def test_add_edge(self, website_one, website_two):
+        assume(website_one != website_two)
+        node1 = Node(website_one)
+        node2 = Node(website_two)
         node1.add_edge(node2)
-        assert node1.edges == {"www.facebook.com": node2}
-        assert node2.edges == {"www.google.com": node1}
+        assert node1.edges == {website_two: node2}
+        assert node2.edges == {website_one: node1}
 
-    def test_get_edge(self):
-        node1 = Node("www.google.com")
-        node2 = Node("www.facebook.com")
+    @given(st.text(min_size=1), st.text(min_size=1))
+    def test_get_edge(self, website_one, website_two):
+        assume(website_one != website_two)
+        node1 = Node(website_one)
+        node2 = Node(website_two)
         node1.add_edge(node2)
-        assert node1.get_edge("www.facebook.com") == node2
-        assert node1.get_edge("www.google.com") == None
-
-    def test_get_edges(self):
-        node1 = Node("www.google.com")
-        node2 = Node("www.facebook.com")
-        node1.add_edge(node2)
-        assert node1.get_edges() == {"www.facebook.com": node2}
+        assert node1.get_edge(website_two) == node2
+        assert node1.get_edge(website_one) == None
     
-    def test_get_number_of_edges(self):
-        node1 = Node("www.google.com")
-        node2 = Node("www.facebook.com")
+    @given(st.text(min_size=1), st.text(min_size=1))
+    def test_get_edges(self, website_one, website_two):
+        assume(website_one != website_two)
+        node1 = Node(website_one)
+        node2 = Node(website_two)
+        node1.add_edge(node2)
+        assert node1.get_edges() == {website_two: node2}
+    
+    @given(st.text(min_size=1), st.text(min_size=1))
+    def test_get_number_of_edges(self, website_one, website_two):
+        assume(website_one != website_two)
+        node1 = Node(website_one)
+        node2 = Node(website_two)
         node1.add_edge(node2)
         assert node1.get_number_of_edges() == 1
 
-    def test_get_website(self):
-        node = Node("www.google.com")
-        assert node.get_website() == "www.google.com"
+    @given(st.text())
+    def test_get_website(self, website):
+        node = Node(website)
+        assert node.get_website() == website
 
-    def test_dunder_str(self):
-        node = Node("www.google.com")
-        assert str(node) == "www.google.com"
-
+    @given(st.text())
+    def test_dunder_str(self, website):
+        node = Node(website)
+        assert str(node) == website
 
 if __name__ == "__main__":
     pytest.main()
