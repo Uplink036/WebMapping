fd521177af28f34aedd2010c767f80678f98c6cc
Uplink <olsj21@student.bth.se>
2025-02-02 22:10:12 +0100

    Chang structure to a more cloud look

--- containers/crawler/src/webmap/database.py
+++ containers/crawler/src/webmap/database.py
@@ -1,8 +1,16 @@
 import pymongo
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def init_db():
     clint = pymongo.MongoClient("mongodb://database:27017/")
     db = clint["webmap"]
+    scrub_db = os.getenv('SCRUB_DB')
+    if int(scrub_db) == 1:
+        clint.drop_database("webmap")
+        db = clint["webmap"]
     return db
 
 def init_node_collection(db):
