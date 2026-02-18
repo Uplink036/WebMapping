8abbcd49029751e21ee8949e2385e6b47cc4a40f
Uplink036 <olsj21@student.bth.se>
2026-02-18 11:00:49 +0000

    Add fullscreen logic to ScreenshotCapture

--- src/webmap/screenshot/capture.py
+++ src/webmap/screenshot/capture.py
@@ -1,3 +1,7 @@
+import time
+from PIL import Image
+from io import BytesIO
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -26,11 +30,45 @@ class ScreenshotCapture:
         """Take screenshot of URL and return as bytes."""
         try:
             self.driver.get(url)
-            screenshot_png = self.driver.get_screenshot_as_png()
-            return screenshot_png
+            return self._fullpage_screenshot()
         except Exception as e:
             print(f"Screenshot Error: taking screenshot of {url}: {e}")
             return None
+        
+    def _fullpage_screenshot(self, scroll_delay=0.3) -> bytes:
+        """
+        Takes a fullscreen pageshot
+        """
+        device_pixel_ratio = self.driver.execute_script('return window.devicePixelRatio')
+
+        total_height = self.driver.execute_script('return document.body.parentNode.scrollHeight')
+        viewport_height = self.driver.execute_script('return window.innerHeight')
+        total_width = self.driver.execute_script('return document.body.offsetWidth')
+        viewport_width = self.driver.execute_script("return document.body.clientWidth")
+
+        assert(viewport_width == total_width)
+
+        offset = 0
+        slices = {}
+        while offset < total_height:
+            if offset + viewport_height > total_height:
+                offset = total_height - viewport_height
+
+            self.driver.execute_script('window.scrollTo({0}, {1})'.format(0, offset))
+            time.sleep(scroll_delay)
+
+            img = Image.open(BytesIO(self.driver.get_screenshot_as_png()))
+            slices[offset] = img
+
+            offset = offset + viewport_height
+
+        stitched_image = Image.new('RGB', (total_width * device_pixel_ratio, total_height * device_pixel_ratio))
+        for offset, image in slices.items():
+            stitched_image.paste(image, (0, offset * device_pixel_ratio))
+        img_byte_arr = BytesIO()
+        stitched_image.save(img_byte_arr, format='PNG')
+        img_byte_arr = img_byte_arr.getvalue()
+        return img_byte_arr
 
     def capture_and_save(self, url: str) -> bool:
         """Take screenshot and save to database."""
